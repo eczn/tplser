5946b9c6bb2f02cf55098e9731ec8e70f61e6311
eczn <1975845422@qq.com>
2017-08-05 16:04:35 +0800

    tplGlobal

--- src/tplGlobal.js
+++ src/tplGlobal.js
@@ -1,9 +1,35 @@
 // tplGlobal
 var tplGlobal = {
+	// 实数
 	"+": (a, b) => a + b, 
 	"-": (a, b) => a - b, 
 	"/": (a, b) => a / b, 
-	"*": (a, b) => a * b 
+	"*": (a, b) => a * b,
+
+	// 比较
+	">": (a, b) => a > b, 
+	"<": (a, b) => a < b, 
+	">=": (a, b) => a >= b, 
+	"<=": (a, b) => a <= b, 
+	"==": (a, b) => a == b, 
+	"===": (a, b) => a === b, 
+
+	// 逻辑 
+	"&": (a, b) => a & b, 
+	"|": (a, b) => a | b, 
+	"~": a => ~a, 
+	"!": a => !a, 
+
+	// 谓词 
+	"&&": (a, b) => a && b, 
+	"||": (a, b) => a || b, 
+
+	// 字符串 / 数组相关 
+	"toLowerCase": a => a.toLowerCase(), 
+	"toUpperCase": a => a.toUpperCase(), 
+	"split": (str, sep) => str.split(sep), 
+	"join": (arr, sep) => arr.join(sep),
+	"trim": str => str.trim()
 }; 
 
 module.exports = tplGlobal; 
