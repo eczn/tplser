bb3e89deb4cf9ffb5681f75bd5d2f1efd5258964
eczn <1975845422@qq.com>
2017-09-15 20:32:35 +0800

    添加 let 和 getPropertyOf

--- src/tplGlobal.js
+++ src/tplGlobal.js
@@ -29,7 +29,37 @@ var tplGlobal = {
 	"toUpperCase": a => a.toUpperCase(), 
 	"split": (str, sep) => str.split(sep), 
 	"join": (arr, sep) => arr.join(sep),
-	"trim": str => str.trim()
+	"trim": str => str.trim(), 
+
+	// 取得属性 
+	getPropertyOf: function(){
+		let args = Array.from(arguments); 
+
+		let main = args[0]; 
+		let names = args.slice(1); 
+
+		return names.reduce((acc, name) => {
+			return acc[name]; 
+		}, main); 
+	}, 
+	// let 定义变量别名  
+	let: function(){
+		let args = Array.from(arguments);
+		let key = args[0]; 
+		let values = args.slice(1); 
+		let first = values[0]; 
+
+		if (typeof first === 'function'){
+			this[key] = first.apply(
+				this,
+				values.slice(1)
+			); 
+		} else {
+			this[key] = first; 
+		}
+
+		return ''; 
+	}
 }; 
 
 module.exports = tplGlobal; 
